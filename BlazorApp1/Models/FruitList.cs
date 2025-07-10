14700545ec35cbb535e2d3c038ca62c2291f86ef
DouaaDaoudi <daoudi_douaa@hotmail.com>
2025-07-10 15:14:32 -0400

    Add project files.

---
+++ BlazorApp1/Models/FruitList.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp1.Models
+{
+    public class FruitList
+    {
+        public string Name { get; set; } ="";
+        public string Available { get; set; } = "";
+        public string Season { get; set; } = "";
+    }
+}
---
+++ BlazorApp1/NewFolder/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using BlazorApp1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp1.NewFolder
+{
+    public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<FruitList> Fruits { get; set; } = null!;
+    }
+}
